66c024a1770bcb95c77105136ff33b70e7725962
vincentl2023 <129344427+vincentl2023@users.noreply.github.com>
2023-06-27 17:31:44 -0500

    Create main2.tf

---
+++ main2.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+    region = "us-east-2"
+}
+
+data "aws_security_group" "rds-ec2-7" {
+  name = "rds-ec2-7
+
+data "aws_security_group" "rds-ec2-8" {
+  name = "rds-ec2-8"
+}
+
+resource "aws_db_instance" "myRDS" {
+  engine                = "mysql"
+  instance_class        = "db.t3.micro"
+  allocated_storage     = 20
+  storage_type          = "gp2"
+  identifier            = "database-2"
+  db_name                  = "purchases"
+  username              = "admin"
+  password              = "vl10055480"
+  port                  = 3306
+  skip_final_snapshot   = true
+  publicly_accessible  = true
+  snapshot_identifier   = "database-2-mysql"
+
+  vpc_security_group_ids = [
+    data.aws_security_group.rds-ec2-7.id,
+    data.aws_security_group.rds-ec2-8.id
+  ]
+  
+  availability_zone = "us-east-2c"
+}
