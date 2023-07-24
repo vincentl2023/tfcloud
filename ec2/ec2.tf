b5910b7797fdf4b6478a4adb26e769766d15e275
vincentl2023 <129344427+vincentl2023@users.noreply.github.com>
2023-07-24 16:56:16 -0500

    Update ec2.tf

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,1 +1,12 @@
+resource "aws_instance" "web" {
+  ami              = "ami-046f6df52d1930356"
+  instance_type    = "t2.micro"
+  security_groups  = ["default"]
+  user_data        = file("server-script.sh")
+  tags = {
+    Name = "Production Refund Rangers"
+  }
 
+  iam_instance_profile = "EC2CodeDeploy"
+
+}
