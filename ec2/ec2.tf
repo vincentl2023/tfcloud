ab3bdda73c39f2db03683dc0e5471b719c17804c
vincentl2023 <129344427+vincentl2023@users.noreply.github.com>
2024-02-01 11:40:56 -0600

    keep it simple.

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -6,32 +6,11 @@ resource "aws_instance" "web" {
   key_name         = "RR-Production"
   user_data        = file("server-script.sh")
   tags = {
-    Name                      = "TNonProductionWebServer"
-    "aws:autoscaling:group"  = aws_autoscaling_group.web.name  # Reference to the existing Auto Scaling Group name
+    Name = "TNonProductionWebServer"
+    "aws:autoscaling:group"  = "DEMOasg"  # Add Auto Scaling Group name as a tag
   }
 
   iam_instance_profile = "EC2CodeDeploy"
 }
 
-resource "aws_autoscaling_group" "web" {
-  name                = "DEMOasg"
-  desired_capacity    = 2
-  max_size            = 3
-  min_size            = 1
-  vpc_zone_identifier = ["subnet-070dc6bbcf6ff035e", "subnet-0f130ccfac1f9a868", "subnet-0142b0c4b6b31d803"]  # Replace with your subnet IDs
-  health_check_type   = "EC2"
-  health_check_grace_period = 300
-
-  tag {
-    key                 = "Name"
-    value               = "TNonProductionWebServer"
-    propagate_at_launch = true
-  }
-
-  tag {
-    key                 = "Environment"
-    value               = "Demo"
-    propagate_at_launch = true
-  }
-}
 
